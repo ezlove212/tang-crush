092d51993ae2dd011a734165b90309847119a5ac
ezlove212 <85699350+ezlove212@users.noreply.github.com>
2021-06-11 10:56:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnNo: 'Không, anh là đồ đáng ghét hmm🙍 :3',
     question:'Trên thế giới này có hơn 7 tỉ người mà sao anh lại yêu em 💓',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Đồ đáng ghét này nữa 😤 em cũng yêuu anhh <33333333',
+    reply: 'Đồ đáng ghét này nữa em cũng yêuu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
