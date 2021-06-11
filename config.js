8d9e4b9cf61254bd80189cc8749736667f222203
ezlove212 <85699350+ezlove212@users.noreply.github.com>
2021-06-11 10:54:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    introDesc: `Em là ai từ đâu bước đến nơi đây dịu dàng chân phương
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi 🍓🍓🍓',
+    btnIntro: 'hihi >>💞',
     title: 'Chào mừng em đã đến thủ đô của sự chân thành và tình yêu bao la của anhhh 😙',
-    desc: 'Lại còn chối, con tim thẹn thùng đập lạc lối liên hồi ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, anh là đồ đáng ghét :3',
-    question:'Trên thế giới này có hơn 7 tỉ người mà sao anh lại yêu em <3',
+    desc: 'Lắng nghe anh luyên thuyên về một tình đầu đẹp tựa chiêm bao ',
+    btnYes: 'Vẫn cứ là thích anh <33333',
+    btnNo: 'Không, anh là đồ đáng ghét hmm🙍 :3',
+    question:'Trên thế giới này có hơn 7 tỉ người mà sao anh lại yêu em 💓',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Đồ đáng ghét này nữa 😤 em cũng yêuu anhh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
