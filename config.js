708ce1beaba1f5c2781f9de0f435af84ad23c7b6
ezlove212 <85699350+ezlove212@users.noreply.github.com>
2021-06-11 20:45:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const CONFIG = {
     introDesc: `Em là ai từ đâu bước đến nơi đây dịu dàng chân phương
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi >>💞',
+    btnIntro: 'hihi >💞',
     title: 'Chào mừng em đã đến thủ đô của sự chân thành và tình yêu bao la của anhhh 😙',
     desc: 'Lắng nghe anh luyên thuyên về một tình đầu đẹp tựa chiêm bao ',
     btnYes: 'Vẫn cứ là thích anh <33333',
     btnNo: 'Không, anh là đồ đáng ghét hmm🙍 :3',
-    question:'Trên thế giới này có hơn 7 tỉ người mà sao anh lại yêu em 💓',
+    question:'Trên thế giới này có hơn 7 tỉ người mà sao em lại yêu anh 💓',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Đồ đáng ghét em cũng yêu anh <33333333',
+    reply: 'Cái đồ đáng ghét em cũng yêu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
